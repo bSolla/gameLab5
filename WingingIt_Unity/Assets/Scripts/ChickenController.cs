c122c222b4f95010bc1367479c91bf6d4927af5c
Kine Olsen <kineolsen96@gmail.com>
2019-09-11 15:29:09 +0200

    Started on chicken movement

--- WingingIt_Unity/Assets/Scripts/ChickenController.cs
+++ WingingIt_Unity/Assets/Scripts/ChickenController.cs
@@ -6,30 +6,38 @@ public class ChickenController : MonoBehaviour
 {
     StatusMenu statusMenu;
     public float planeX, planeZ;
-    Vector3 walkPoint;
+    int walkPoint;
+    public float movementSpeed = 5f;
+    public GameObject[] walkingPoints;
 
     void Start()
     {
         planeX = GameObject.FindWithTag("Ground").transform.localScale.x;
         planeZ = GameObject.FindWithTag("Ground").transform.localScale.z;
+
+        walkingPoints = GameObject.FindGameObjectsWithTag("WalkingPoint");
+        walkPoint = Random.Range(0, walkingPoints.Length);
     }
 
     void Update()
     {
         
-        print (walkPoint);
-        if(Vector3.Distance(transform.position, walkPoint) > 0.0001f)
+        print ("Walk point: " + walkPoint);
+        if(Vector3.Distance(transform.position, walkingPoints[walkPoint].transform.position) > 0.1f)
         {
             print ("Distance is far");
-            transform.Translate(walkPoint, Space.Self);
+            // transform.Translate(walkPoint, Space.Self);
+            transform.position += walkingPoints[walkPoint].transform.position * movementSpeed * Time.deltaTime;
             
         }
         else
         {
-            float rndX = Random.Range(0, planeX); print (rndX);
-            float rndZ = Random.Range(0, planeZ);
-            walkPoint = new Vector3(rndX, transform.position.y, rndZ);
+            // float rndX = Random.Range(0, planeX); print (rndX);
+            // float rndZ = Random.Range(0, planeZ);
+            // walkPoint = new Vector3(rndX, transform.position.y, rndZ);
+            walkPoint = Random.Range(0, walkingPoints.Length);
 
+       
         }
         // transform.Translate(walkPoint, )
     }
