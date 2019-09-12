553b48b8faa7a821380dfd39430e3e73c821a4b3
Kine Olsen <kineolsen96@gmail.com>
2019-09-12 12:16:58 +0200

    Chicken Movement low key kinda working

--- WingingIt_Unity/Assets/Scripts/ChickenController.cs
+++ WingingIt_Unity/Assets/Scripts/ChickenController.cs
@@ -6,39 +6,77 @@ public class ChickenController : MonoBehaviour
 {
     StatusMenu statusMenu;
     public float planeX, planeZ;
-    int walkPoint;
+    int currWalkPoint;
     public float movementSpeed = 5f;
     public GameObject[] walkingPoints;
 
+    CharacterController charController;
+
     void Start()
     {
-        planeX = GameObject.FindWithTag("Ground").transform.localScale.x;
-        planeZ = GameObject.FindWithTag("Ground").transform.localScale.z;
+        charController = GetComponent<CharacterController>();
+        currWalkPoint = Random.Range(0, walkingPoints.Length);
+
+        // planeX = GameObject.FindWithTag("Ground").transform.localScale.x;
+        // planeZ = GameObject.FindWithTag("Ground").transform.localScale.z;
 
         walkingPoints = GameObject.FindGameObjectsWithTag("WalkingPoint");
-        walkPoint = Random.Range(0, walkingPoints.Length);
+        // walkPoint = Random.Range(0, walkingPoints.Length);
+        // print ("Walk point: " + walkPoint);
+
     }
 
     void Update()
     {
+        movingPoint();
         
-        print ("Walk point: " + walkPoint);
-        if(Vector3.Distance(transform.position, walkingPoints[walkPoint].transform.position) > 0.1f)
-        {
-            print ("Distance is far");
-            // transform.Translate(walkPoint, Space.Self);
-            transform.position += walkingPoints[walkPoint].transform.position * movementSpeed * Time.deltaTime;
+        // float dist = Vector3.Distance(transform.position, walkingPoints[walkPoint].transform.position);
+        // print ("Dist " +  Mathf.RoundToInt(dist));
+        // if(dist < 0.2f || dist > 3)
+        // {
+        //     // float rndX = Random.Range(0, planeX); print (rndX);
+        //     // float rndZ = Random.Range(0, planeZ);
+        //     // walkPoint = new Vector3(rndX, transform.position.y, rndZ);
+        //     walkPoint = Random.Range(0, walkingPoints.Length);
+        //     print ("New Walk point: " + walkPoint);
             
-        }
-        else
-        {
-            // float rndX = Random.Range(0, planeX); print (rndX);
-            // float rndZ = Random.Range(0, planeZ);
-            // walkPoint = new Vector3(rndX, transform.position.y, rndZ);
-            walkPoint = Random.Range(0, walkingPoints.Length);
+            
+            
+        // }
+        // else
+        // {
+        //     transform.LookAt(walkingPoints[walkPoint].transform);
+        //     Vector3 movement = Vector3.forward * movementSpeed * Time.deltaTime;
+        //     charController.Move(movement);
+
+        //     print ("Distance is far");
+        //     // transform.Translate(walkPoint, Space.Self);
+        //     // transform.position -= walkingPoints[walkPoint].transform.position * movementSpeed * Time.deltaTime;
+
+        //     // print ("Walk point " + walkingPoints[walkPoint].transform.position);
+
 
        
         }
         // transform.Translate(walkPoint, )
+
+    void movingPoint()
+    {
+        Vector3 target = walkingPoints[currWalkPoint].transform.position;
+        target.y = transform.position.y;
+        Vector3 moveDir = target - transform.position;
+        if(moveDir.magnitude < 1f)
+        {
+            // transform.position = target;
+            currWalkPoint = Random.Range(0, walkingPoints.Length);
+        }
+        else
+        {
+            Vector3 lookAtTarget = Vector3.RotateTowards(transform.forward, target, 10000f, 10000f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(lookAtTarget), 5/Time.deltaTime);
+            // transform.rotation = Quaternion.LookRotation(lookAtTarget);
+            charController.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+        }
+
     }
 }
