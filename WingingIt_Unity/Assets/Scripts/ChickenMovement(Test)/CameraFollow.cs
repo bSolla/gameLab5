1295eaa408be4a16da6e3584b0ea9a12b395bd0b
TeresaGJ <teresa.johnsen@hotmail.com>
2019-10-06 14:38:20 +0200

    should be a bit smoother now

--- WingingIt_Unity/Assets/Scripts/ChickenMovement(Test)/CameraFollow.cs
+++ WingingIt_Unity/Assets/Scripts/ChickenMovement(Test)/CameraFollow.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//                           A U T H O R  &  N O T E S
+//                          coded by Teresa, October 2019
+//              zooms in when you clicke the chicken and follows its movement
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraFollow : MonoBehaviour
 {
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//                                V A R I A B L E S 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     public Vector3 originPosition;
-    public Quaternion originRotation;
+    public Quaternion originRotation, newRotation;
     //public Vector3 targetToFollow;
     private float smooth = 5f;
     public bool following = false;
@@ -14,7 +24,10 @@ public class CameraFollow : MonoBehaviour
     public float duration = 1f;
     public float elapsed = 0f;
     public bool transitioning = false;
-    // Start is called before the first frame update
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//                                M E T H O D S 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     void Start()
     {
         originPosition = transform.position;
@@ -27,6 +40,7 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log (following);
         if (transitioning)
         {
             if (following)
@@ -35,12 +49,18 @@ public class CameraFollow : MonoBehaviour
                 followChicken();
                 elapsed += Time.deltaTime / duration;
                 cam.orthographicSize = Mathf.Lerp (7.91f, 2f, elapsed);
+                transform.rotation = Quaternion.Lerp (originRotation, newRotation, elapsed);
             }
             else
             {
                 elapsed += Time.deltaTime / duration;
                 cam.orthographicSize = Mathf.Lerp (2, 7.91f, elapsed);
+                transform.rotation = Quaternion.Lerp (transform.rotation, originRotation, elapsed);
             }
+        }
+        else
+        {
+            return;
         }   
     }
 
@@ -50,6 +70,7 @@ public class CameraFollow : MonoBehaviour
 
         //transform.position = Vector3.Lerp (transform.position, targetToFollow, Time.deltaTime * smooth);
         transform.LookAt(target);
+        newRotation = transform.rotation;
     }
 
     public void startFollowing(Transform targetToFollow)
@@ -66,8 +87,8 @@ public class CameraFollow : MonoBehaviour
         elapsed = 0;
         following = false;
         //cam.orthographicSize = 7.91f;
-        transform.position = originPosition;
-        transform.rotation = originRotation;
+        //transform.position = originPosition;
+        //transform.rotation = originRotation;
 
     }
 }
