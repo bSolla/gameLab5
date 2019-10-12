e57e3b82da33b32d71c9e13e9f55260c3b57c625
TeresaGJ <teresa.johnsen@hotmail.com>
2019-10-12 16:36:42 +0200

    wip

---
+++ WingingIt_Unity/Assets/Scripts/Pellet Drop/FeederMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeederMovement : MonoBehaviour
+{
+    //private Rigidbody2D myRigidbody;
+    public float speed, xBound;
+    public float offset = 0.05f;
+    public bool following = false;
+    public PelletDropManager manager;
+    public ParticleSystem seeds;
+    public List <ParticleCollisionEvent> collisionEvents;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetMouseButtonDown(0))
+      {
+          following = true;
+      }
+
+      if (following)
+      {
+          float dist = transform.position.z - Camera.main.transform.position.z;
+          Vector3 pos = Input.mousePosition;
+          pos.z = dist;
+          pos = Camera.main.ScreenToWorldPoint(pos);
+          pos.y = transform.position.y;
+          transform.position = pos;
+      }
+
+    }
+
+    public void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.tag == "Seeds")
+        {
+            manager.score += manager.scoreGained;
+        }
+    }
+
+    void OnParticleCollision (GameObject other)
+    {
+        Debug.Log ("omg a seed touched me");
+       /*  int numCollisionEvents = seeds.GetCollisionEvents (other, collisionEvents);
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            if (rb)
+            {
+                Vector3 pos = collisionEvents[i].intersection;
+                Vector3 force = collisionEvents[i].velocity * 10;
+                rb.AddForce (force);
+            }
+            i++;
+        } */
+    }
+}
---
+++ WingingIt_Unity/Assets/Scripts/Pellet Drop/PelletDropManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PelletDropManager : MonoBehaviour
+{
+
+    public int score = 0;
+    public int scoreGained = 1;
+    public Text scoreText;
+    public float gameTimer = 0;
+    public Text timeLeft;
+    public Button exitGameButton;
+    public FeederMovement feeder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+        exitGameButton.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft.text = Mathf.Round (gameTimer).ToString();
+        gameTimer -= Time.deltaTime;
+        scoreText.text = ("Score: " + score.ToString());
+
+        if (gameTimer <= 0)
+        {
+            Debug.Log ("ending game");
+            timeLeft.gameObject.SetActive (false);
+            scoreText.gameObject.SetActive (false);
+            exitGameButton.gameObject.SetActive(true);
+            feeder.following = false;
+
+        }
+    }
+
+    void StartGame ()
+    {
+        // do we need this tho idk
+    }
+}
